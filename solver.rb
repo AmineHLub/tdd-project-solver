86d817e2f0b21e234d2bb085d0df690048fe9149
AmineHLub <Amino.bhl@gmail.com>
2021-12-24 16:40:15 +0100

    fix minor isssues (error for negative factorial number)

--- solver.rb
+++ solver.rb
@@ -2,18 +2,20 @@ class Solver
   def factorial(num)
     num_factorial = 1
     if num.negative?
-      "Factorial of negative number can't be calculated"
+      'error ! negative number'
     else
       (1..num).each { |p| num_factorial *= p }
       num_factorial
     end
   end
+
   def reverse(string)
     split_str = string.downcase.chars
     reverse_str = []
     split_str.each { |str| reverse_str.unshift(str) }
     reverse_str.join
   end
+
   def fizzbuzz(num)
     if (num % 3).zero? && (num % 5).zero?
       'fizzbuzz'
@@ -25,3 +27,4 @@ class Solver
       num.to_s
     end
   end
+end
